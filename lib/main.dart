ab1952f18dc78b391dccce9b1679b453db32e9d4
Haruki1090 <haruharu.com.109607@gmail.com>
2025-02-09 03:39:00 +0900

    .env読み込み

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,18 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notion_gtd_manager/src/config/theme.dart';
 import 'package:notion_gtd_manager/src/screens/auth_wrapper.dart';
+import 'package:notion_gtd_manager/src/services/notion_api_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
-  runApp(const ProviderScope(child: NotionGTDManagerApp()));
+  runApp(ProviderScope(overrides: [
+    // ここで notionApiServiceProvider をオーバーライドして、アクセストークンをセットしてもよい
+    notionApiServiceProvider.overrideWithValue(
+      NotionApiService()..setAccessToken(dotenv.env['NOTION_API_KEY']),
+    ),
+  ], child: const NotionGTDManagerApp()));
 }
 
 class NotionGTDManagerApp extends ConsumerWidget {
